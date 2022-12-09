2ac86fcb5d6b8458acce7e502071546f4038fb16
Hastred45 <hastred45@yandex.ru>
2022-12-09 14:58:00 +0500

    review_8

--- pep_parse/middlewares.py
+++ pep_parse/middlewares.py
@@ -27,10 +27,22 @@ class PepParseSpiderMiddleware:
         spider.logger.info('Spider opened: %s' % spider.name)
 
 
-class PepParseDownloaderMiddleware(PepParseSpiderMiddleware):
+class PepParseDownloaderMiddleware:
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        return s
 
     def process_request(self, request, spider):
         return None
 
     def process_response(self, request, response, spider):
         return response
+
+    def process_exception(self, request, exception, spider):
+        pass
+
+    def spider_opened(self, spider):
+        spider.logger.info('Spider opened: %s' % spider.name)
